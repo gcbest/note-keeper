07b1c428d3bb5b7551a2be48e298c84f5619b6b1
Godfrey Best <gcb246@stern.nyu.edu>
2017-02-02 23:10:14 -0500

    removed node modules

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+
+require('./app/routes')(app, {});
+app.listen(PORT, () => {
+	console.log("App running on port: " + PORT);
+}
