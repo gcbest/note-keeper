87b9e2be2849e38c7d11a2afee72521f5136ccd5
Godfrey Best <gcb246@stern.nyu.edu>
2017-02-12 20:44:43 -0500

    to do list added

--- app/public/script.js
+++ app/public/script.js
@@ -43,7 +43,24 @@
 // }
 
 
-// var axios = require('axios');
+//////////////////////////////////////////////////////////////////////////////////// 
+/*
+TO DO LIST
+
+Edit tasks
+Trigger word to start writing task
+If you click button when already recording it doesnt break
+
+Login
+
+Front End
+   separate tasks
+
+
+
+*/
+
+//////////////////////////////////////////////////////////////////////////////////// 
 
 var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
 var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
